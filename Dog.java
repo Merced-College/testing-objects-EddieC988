f37096624d411f0a57c2dad5f0847d42ed32ab6e
EddieC988 <eddie988@icloud.com>
2026-06-09 00:49:39 +0000

    starting

--- Dog.java
+++ Dog.java
@@ -1,1 +1,13 @@
-//write your code here
+//Eduardo Cerna
+//6/8/26
+//Dog class for my Dog class
+
+public class Dog {
+
+    //data variables
+    public String name;
+    public int age;
+    public String breed;
+
+
+} //end Dog class
